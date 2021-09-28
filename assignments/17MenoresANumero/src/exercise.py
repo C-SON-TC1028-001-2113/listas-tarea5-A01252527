adb01836dd0737cf857f11dcde33cbe073f8ddbe
A01252527 <a01252527@itesm.mx>
2021-09-28 06:25:32 +0000

    alpha

--- assignments/17MenoresANumero/src/exercise.py
+++ assignments/17MenoresANumero/src/exercise.py
@@ -1,9 +1,16 @@
 
-
 def main():
-
-
-
+    a = int(input(''))
+    aa = int(input(''))
+    aaa = a*aa
+    lista=[]
+    i=1
+    while i<=aaa:
+        i=i+1
+        aaaa = int(input(''))
+        if aaaa<10:
+            lista.append(aaaa)
+    print(lista)
 
 if __name__=='__main__':
     main()
